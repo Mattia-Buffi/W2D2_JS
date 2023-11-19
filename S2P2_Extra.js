63b96da51bc2b035e4db7b4bf0b326f8f6400101
Mattia Buffello <buffello@me.com>
2023-11-19 17:53:08 +0100

    eliminati doppi controlli

--- S2P2_Extra.js
+++ S2P2_Extra.js
@@ -32,7 +32,7 @@ for (let index = 0; index < array.length; index++) {
         array.splice(index,1);
     } else continue
 }
-console.log(array)
+console.log(array);
 
 //? Ex.4 (Hard) - Dato un array di numeri interi (nums) ed un numero intero (target), 
 //                  ritornare i 2 indici degli elementi nell’array nums che, sommati, restituiscano il numero target.
@@ -43,26 +43,26 @@ console.log(array)
 
 // nums [0, 1, 2, 3, 4, 5, 6]
 // Suggerimento: Sapevi che i cicli for possono essere innestati?
-let nums = [4, 9, 11, 14]
-let target = 22
-let targetIndex=[]
-let finded=false
+let nums = [4, 9, 11, 13];
+let target = 22;
+let targetIndex=[];
+let finded=false;
 
 
 
 for (let i = 0; i < nums.length; i++) {
     if(finded) break
     else{
-    for (let index = 0; index < nums.length; index++) {
+    for (let index = i; index < nums.length; index++) {
         if (i===index) continue
         else{
             if(target==(nums[i]+nums[index])){
-                targetIndex.push(i,index)
-                console.log(targetIndex)
-                finded=true
+                targetIndex.push(i,index);
+                console.log(targetIndex);
+                finded=true;
                 break
             } else continue}  
     } 
     }   
 }
-if(!finded) console.log('Target no finded')
+if(!finded) console.log('Target no finded');
